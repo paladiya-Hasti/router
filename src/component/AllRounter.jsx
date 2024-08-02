381e32e8706c63d669298e0fd4e1f7a32f6efacd
hasti <hastipaladiya2001@gmail.com>
2024-08-02 14:48:19 +0530

    navbar

--- src/component/AllRounter.jsx
+++ src/component/AllRounter.jsx
@@ -3,15 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Product from "../Page/Product";
 import Sign from "../Page/Sign";
 import Login from "../Page/Login";
+import Home from "../Page/Home";
+import Error from "../Page/Error";
 
 const AllRounter = () => {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Home/>}/> */}
-        <Route path="/Product"   className="box"element={<Product />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Product" className="box" element={<Product />} />
         <Route path="/Sign" element={<Sign />} />
-        <Route path="/Login" element={<Login/>} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<Error/>}/>
       </Routes>
     </div>
   );
